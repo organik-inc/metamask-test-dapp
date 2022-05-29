1480ad710966d1e55e3ce04a608e1badaf325ec7
organik-inc <luis@organik.express>
2022-05-29 17:22:31 +0000

    Cleaned the INDEX js

--- src/index.js
+++ src/index.js
@@ -25,51 +25,7 @@ const onboardButton = document.getElementById('connectButton');
 const getAccountsButton = document.getElementById('getAccounts');
 const getAccountsResults = document.getElementById('getAccountsResult');
 
-// Permissions Actions Section
-/** *!/
-const requestPermissionsButton = document.getElementById('requestPermissions');
-const getPermissionsButton = document.getElementById('getPermissions');
-const permissionsResult = document.getElementById('permissionsResult');
-/** */
-// Contract Section
-/** *!/
-const deployButton = document.getElementById('deployButton');
-const depositButton = document.getElementById('depositButton');
-const withdrawButton = document.getElementById('withdrawButton');
-const contractStatus = document.getElementById('contractStatus');
-const deployFailingButton = document.getElementById('deployFailingButton');
-const sendFailingButton = document.getElementById('sendFailingButton');
-const failingContractStatus = document.getElementById('failingContractStatus');
-/** */
 
-/** !/
-// Collectibles Section
-const deployCollectiblesButton = document.getElementById(
-  'deployCollectiblesButton',
-);
-const mintButton = document.getElementById('mintButton');
-const mintAmountInput = document.getElementById('mintAmountInput');
-const collectiblesStatus = document.getElementById('collectiblesStatus');
-/** */
-// Send Eth Section
-/** *!/
-const sendButton = document.getElementById('sendButton');
-const sendEIP1559Button = document.getElementById('sendEIP1559Button');
-/** */
-
-// Send Tokens Section
-/** *!/
-const tokenAddress = document.getElementById('tokenAddress');
-const createToken = document.getElementById('createToken');
-const watchAsset = document.getElementById('watchAsset');
-const transferTokens = document.getElementById('transferTokens');
-const approveTokens = document.getElementById('approveTokens');
-const transferTokensWithoutGas = document.getElementById(
-  'transferTokensWithoutGas',
-);
-const approveTokensWithoutGas = document.getElementById(
-  'approveTokensWithoutGas',
-);
 /** */
 // Encrypt / Decrypt Section
 const getEncryptionKeyButton = document.getElementById(
@@ -82,50 +38,8 @@ const encryptionKeyDisplay = document.getElementById('encryptionKeyDisplay');
 const ciphertextDisplay = document.getElementById('ciphertextDisplay');
 const cleartextDisplay = document.getElementById('cleartextDisplay');
 
-// Ethereum Signature Section
-const ethSign = document.getElementById('ethSign');
-const ethSignResult = document.getElementById('ethSignResult');
-const personalSign = document.getElementById('personalSign');
-const personalSignResult = document.getElementById('personalSignResult');
-const personalSignVerify = document.getElementById('personalSignVerify');
-const personalSignVerifySigUtilResult = document.getElementById(
-  'personalSignVerifySigUtilResult',
-);
-const personalSignVerifyECRecoverResult = document.getElementById(
-  'personalSignVerifyECRecoverResult',
-);
-const signTypedData = document.getElementById('signTypedData');
-const signTypedDataResult = document.getElementById('signTypedDataResult');
-const signTypedDataVerify = document.getElementById('signTypedDataVerify');
-const signTypedDataVerifyResult = document.getElementById(
-  'signTypedDataVerifyResult',
-);
-const signTypedDataV3 = document.getElementById('signTypedDataV3');
-const signTypedDataV3Result = document.getElementById('signTypedDataV3Result');
-const signTypedDataV3Verify = document.getElementById('signTypedDataV3Verify');
-const signTypedDataV3VerifyResult = document.getElementById(
-  'signTypedDataV3VerifyResult',
-);
-const signTypedDataV4 = document.getElementById('signTypedDataV4');
-const signTypedDataV4Result = document.getElementById('signTypedDataV4Result');
-const signTypedDataV4Verify = document.getElementById('signTypedDataV4Verify');
-const signTypedDataV4VerifyResult = document.getElementById(
-  'signTypedDataV4VerifyResult',
-);
-
 // Send form section
 const fromDiv = document.getElementById('fromInput');
-const toDiv = document.getElementById('toInput');
-const type = document.getElementById('typeInput');
-const amount = document.getElementById('amountInput');
-const gasPrice = document.getElementById('gasInput');
-const maxFee = document.getElementById('maxFeeInput');
-const maxPriority = document.getElementById('maxPriorityFeeInput');
-const data = document.getElementById('dataInput');
-const gasPriceDiv = document.getElementById('gasPriceDiv');
-const maxFeeDiv = document.getElementById('maxFeeDiv');
-const maxPriorityDiv = document.getElementById('maxPriorityDiv');
-const submitFormButton = document.getElementById('submitForm');
 
 // Miscellaneous
 const addEthereumChain = document.getElementById('addEthereumChain');
@@ -153,34 +67,10 @@ const initialize = async () => {
   let accountButtonsInitialized = false;
 
   const accountButtons = [
-    // deployButton,
-    // depositButton,
-    // withdrawButton,
-    // deployCollectiblesButton,
-    // mintButton,
-    // mintAmountInput,
-    // deployFailingButton,
-    // sendFailingButton,
-    // sendButton,
-    // createToken,
-    // watchAsset,
-    // transferTokens,
-    // approveTokens,
-    // transferTokensWithoutGas,
-    // approveTokensWithoutGas,
     getEncryptionKeyButton,
     encryptMessageInput,
     encryptButton,
     decryptButton,
-    // ethSign,
-    // personalSign,
-    // personalSignVerify,
-    // signTypedData,
-    // signTypedDataVerify,
-    // signTypedDataV3,
-    // signTypedDataV3Verify,
-    // signTypedDataV4,
-    // signTypedDataV4Verify,
   ];
 
   const isMetaMaskConnected = () => accounts && accounts.length > 0;
@@ -218,17 +108,7 @@ const initialize = async () => {
       }
       clearTextDisplays();
     } else {
-      // deployCollectiblesButton.disabled = false;
-      // sendButton.disabled = false;
-      // createToken.disabled = false;
-      // personalSign.disabled = false;
-      // signTypedData.disabled = false;
       getEncryptionKeyButton.disabled = false;
-      // ethSign.disabled = false;
-      // personalSign.disabled = false;
-      // signTypedData.disabled = false;
-      // signTypedDataV3.disabled = false;
-      // signTypedDataV4.disabled = false;
     }
 
     if (isMetaMaskInstalled()) {
@@ -284,24 +164,6 @@ const initialize = async () => {
       return;
     }
     accountButtonsInitialized = true;
-    /** *!/
-    sendEIP1559Button.onclick = async () => {
-      const result = await ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [
-          {
-            from: accounts[0],
-            to: '0x0c54FcCd2e384b4BB6f2E405Bf5Cbc15a017AaFb',
-            value: '0x0',
-            gasLimit: '0x5028',
-            maxFeePerGas: '0x2540be400',
-            maxPriorityFeePerGas: '0x3b9aca00',
-          },
-        ],
-      });
-      console.log(result);
-    };
-    /** */
 
     /**
      * Permissions
@@ -384,9 +246,6 @@ const initialize = async () => {
     accounts = newAccounts;
     accountsDiv.innerHTML = accounts;
     fromDiv.value = accounts;
-    // gasPriceDiv.style.display = 'block';
-    // maxFeeDiv.style.display = 'none';
-    // maxPriorityDiv.style.display = 'none';
     if (isMetaMaskConnected()) {
       initializeAccountButtons();
     }
@@ -406,15 +265,11 @@ const initialize = async () => {
   function handleEIP1559Support(supported) {
     if (supported && Array.isArray(accounts) && accounts.length >= 1) {
       /** *!/
-      sendEIP1559Button.disabled = false;
-      sendEIP1559Button.hidden = false;
-      sendButton.innerText = 'Send Legacy Transaction';
+      A
       /** */
     } else {
       /** *!/
-      sendEIP1559Button.disabled = true;
-      sendEIP1559Button.hidden = true;
-      sendButton.innerText = 'Send';
+      B
       /** */
     }
   }
